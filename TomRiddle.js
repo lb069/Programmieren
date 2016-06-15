5eaeb8bc1e48b58e89e6f631d52fc83353fc4bf2
SandraDiamantis <constantin@Constantins-MBP.fritz.box>
2016-06-15 16:21:12 +0200

    commit

--- TomRiddle.js
+++ TomRiddle.js
@@ -1,12 +1,33 @@
-function myFunction() {
-goToXY(20,20);
+window.onload = init;
 
-for (var i = 1; i <= 200; i++) {
-  move(i);
-  turnRight(121);
+var gCanvas;
+var g2d;
+var width = 854;
+var height = 480;
 
+var title = "Tom Riddle' Diary";
+var gameState = 0;
+
+function init() {
+	gCanvas = document.getElementById("gameCanvas");
+	gCanvas.width = width;
+	gCanvas.height = height ;
+
+	g2d = gCanvas.getContext("2d");
+	g2d.imageSmoothingEnabled = false;
+	g2d.webkitImageSmoothingEnabled = false;
+	g2d.mozImageSmoothingENabled = false;
+
+	console.log("Game canvas initialized");
+	draw();
 }
-}
 
-sandra
-lara
+function draw() {
+	if (gameState == 0) { 
+		g2d.font = "20px Verdana";
+		g2d.fillStyle = "#FFFFFF";
+		g2d.fillText(title, (width / 2) - (g2d.measureText(title).width / 2, 30;
+	}
+
+	console.log("Game drawn successfully");
+}
